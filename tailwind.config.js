4f5f8f12763cb48c4bec346603e564a02e9d7903
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2023-12-05 15:29:40 +0500

    setup base style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,34 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      satoshi: ['Satoshi', 'sans-serif'],
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: {
+          50: '#e6f6f7',
+          100: '#ccecee',
+          200: '#99d9dd',
+          300: '#66c6cc',
+          400: '#33b3bb',
+          500: '#00a0aa',
+          600: '#008088',
+          700: '#006066',
+          800: '#004044',
+          900: '#002022',
+        },
+        secondary: '#FFC567',
+      },
+      width: {
+        container: '1120px',
+      },
     },
   },
   plugins: [],
-}
+};
